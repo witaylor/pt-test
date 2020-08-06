4414c370774e2d9d4c004fd1175cb7461157236d
Will Taylor <wat23@bath.ac.uk>
2020-08-06 15:50:42 +0100

    rewrite

---
+++ src/scripts/main.js
@@ -0,0 +1,179 @@
+const canvas = document.getElementById('results-canvas');
+const ctx = canvas.getContext('2d');
+
+function hide(element) {
+  element.hidden = true;
+  element.classList.add('hidden');
+}
+
+function unhide(element) {
+  element.hidden = false;
+  element.classList.remove('hidden');
+}
+
+async function logAnalytics(dataUrl) {
+  console.log('analytics');
+
+  const uploadRes = fetch('https://safe-refuge-39389.herokuapp.com/saveUri', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ url: dataUrl }),
+  });
+
+  console.log('(A) Analytics result:', uploadRes);
+  return uploadRes;
+}
+
+async function processPose(canvasElement) {
+  console.log('(P) Loading PoseNet.');
+  const net = await posenet.load();
+
+  console.log('(P) Estimating pose.');
+  const pose = await net.estimateSinglePose(canvasElement, {
+    flipHorizontal: false,
+  });
+
+  console.log(
+    `(P) Successfully estimated pose, c=${`${pose.score}`.slice(0, 4)}`
+  );
+  return pose;
+}
+
+function drawLineBetweenPoints(pointOne, pointTwo) {
+  ctx.lineWidth = 6;
+  ctx.strokeStyle = '#FF0000';
+  ctx.beginPath();
+  ctx.moveTo(pointOne.x, pointOne.y);
+  ctx.lineTo(pointTwo.x, pointTwo.y);
+  ctx.stroke();
+}
+
+function drawLineBetweenParts(partNameOne, partNameTwo, pointsList) {
+  const partOne = pointsList.find((it) => it.part === partNameOne);
+  const partTwo = pointsList.find((it) => it.part === partNameTwo);
+
+  drawLineBetweenPoints(partOne.position, partTwo.position);
+}
+
+function drawPoseSkeleton(pointsList) {
+  // draw dots
+  pointsList.forEach((kp) => {
+    const { x, y } = kp.position;
+
+    ctx.fillStyle = '#FF0000';
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2 * Math.PI);
+    ctx.fill();
+  });
+
+  // draw skeleton outline
+  drawLineBetweenParts('leftAnkle', 'leftKnee', pointsList);
+  drawLineBetweenParts('leftKnee', 'leftHip', pointsList);
+
+  drawLineBetweenParts('rightAnkle', 'rightKnee', pointsList);
+  drawLineBetweenParts('rightKnee', 'rightHip', pointsList);
+
+  drawLineBetweenParts('leftHip', 'rightHip', pointsList);
+  drawLineBetweenParts('leftHip', 'leftShoulder', pointsList);
+  drawLineBetweenParts('rightHip', 'rightShoulder', pointsList);
+  drawLineBetweenParts('leftShoulder', 'rightShoulder', pointsList);
+
+  drawLineBetweenParts('leftShoulder', 'leftElbow', pointsList);
+  drawLineBetweenParts('leftElbow', 'leftWrist', pointsList);
+
+  drawLineBetweenParts('rightShoulder', 'rightElbow', pointsList);
+  drawLineBetweenParts('rightElbow', 'rightWrist', pointsList);
+
+  drawLineBetweenParts('leftEar', 'leftEye', pointsList);
+  drawLineBetweenParts('leftEye', 'nose', pointsList);
+  drawLineBetweenParts('nose', 'rightEye', pointsList);
+  drawLineBetweenParts('rightEye', 'rightEar', pointsList);
+
+  const nose = pointsList.find((it) => it.part === 'nose');
+  const leftShoulder = pointsList.find((it) => it.part === 'leftShoulder')
+    .position;
+  const rightShoulder = pointsList.find((it) => it.part === 'rightShoulder')
+    .position;
+  const shoulderMidpoint = {
+    x: (leftShoulder.x + rightShoulder.x) / 2,
+    y: (leftShoulder.y + rightShoulder.y) / 2,
+  };
+  drawLineBetweenPoints(nose.position, shoulderMidpoint);
+}
+
+function handleImageUpload(dataUrl) {
+  console.log('1. Image uploaded');
+
+  Promise.all([logAnalytics(dataUrl), processPose(canvas)]).then((res) => {
+    // hide the calculating screen
+    hide(document.getElementById('pose-calc-screen'));
+
+    const [analyticRes, poseRes] = res;
+
+    console.log('AR', analyticRes);
+    console.log('PR', poseRes);
+
+    drawPoseSkeleton(poseRes.keypoints);
+
+    hide(document.getElementById('upload-instruction-container'));
+    hide(document.querySelector('#uploaded-img-container h3'));
+
+    unhide(document.getElementById('results-info-container'));
+  });
+}
+
+let uploadedImage = null;
+const imageUploaderInput = document.getElementById('img-uploader');
+
+imageUploaderInput.addEventListener('change', (e) => {
+  console.log('img added', e.target.files[0]);
+  uploadedImage = e.target.files[0];
+
+  const reader = new FileReader();
+  reader.onload = function (event) {
+    const dataUrl = event.target.result;
+
+    const canvasBackground = new Image();
+    canvasBackground.src = dataUrl;
+
+    canvasBackground.onload = function (e) {
+      console.log('ee:', e);
+      canvas.width = canvasBackground.width;
+      canvas.height = canvasBackground.height;
+
+      ctx.drawImage(
+        canvasBackground,
+        0,
+        0,
+        canvasBackground.width,
+        canvasBackground.height,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+    };
+
+    hide(document.getElementById('example-img-container'));
+    unhide(document.getElementById('uploaded-img-container'));
+  };
+  reader.readAsDataURL(uploadedImage);
+});
+
+document.getElementById('upload-btn').addEventListener('click', () => {
+  // show the calculating screen
+  unhide(document.getElementById('pose-calc-screen'));
+
+  const reader = new FileReader();
+  reader.onload = function (event) {
+    const dataUrl = event.target.result;
+    handleImageUpload(dataUrl);
+  };
+
+  // todo - check & verify uploaded image, handle no upload nicely
+
+  reader.readAsDataURL(uploadedImage);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    maxHeight: {
+      '0': '0',
+      '256': '256px',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
